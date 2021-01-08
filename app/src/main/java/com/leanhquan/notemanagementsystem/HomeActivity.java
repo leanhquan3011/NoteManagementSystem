a089b1a8244d3cb41405e8e2c63875eebd6b4e4d
leanhquan3011 <leanhquan301198@gmail.com>
2021-01-08 20:22:00 +0700

    finish login and register: todo->disable sharedreperencse

---
+++ app/src/main/java/com/leanhquan/notemanagementsystem/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.leanhquan.notemanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //todo: disable SharedReperences
+    }
+}
